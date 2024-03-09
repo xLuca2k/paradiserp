2a69ab065360279ae566979d6275a0d2c7004030
Luca2k <luca2010meze@gmail.com>
2024-03-09 15:33:09 +0200

    EMANUEL
    EMANUEL

---
+++ start.cmd
@@ -0,0 +1,3 @@
+﻿@echo off
+start D:\paradiserp\artefacts_core\FxServer.exe +exec server.cfg +set sv_enforceGameBuild 2802 +set onesync on +set onesync_distanceCullVehicles true +set onesync_distanceCulling true
+exit
